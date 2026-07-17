38b66f7c3de4070f30961032035800d17b5d621a
rbc33 <ricardobenthem@gmail.com>
2026-07-17 18:12:07 +0200

    Initial Commit

---
+++ Purrl/PurrlApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurrlApp.swift
+//  Purrl
+//
+//  Created by ric on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct PurrlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
